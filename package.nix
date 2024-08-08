119b046e32a9736bd9d3f90aeefae7b490f01b42
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-08-09 00:49:31 +0200

    linked awk and lsblk to packages

--- package.nix
+++ package.nix
@@ -12,6 +12,9 @@ pkgs.stdenv.mkDerivation {
   postPatch = ''
     substituteInPlace src/argonsysinfo.py --replace '/usr/sbin/smartctl' ${pkgs.smartmontools}/bin/smartctl
     substituteInPlace src/argonsysinfo.py --replace '/usr/sbin/hddtemp' ${pkgs.hddtemp}/bin/hddtemp
+
+    substituteInPlace src/argonsysinfo.py --replace 'awk' ${pkgs.gawk}/bin/awk
+    substituteInPlace src/argonsysinfo.py --replace 'lsblk' ${pkgs.util-linux}/bin/lsblk
   '';
 
   installPhase = let
