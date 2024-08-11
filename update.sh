9de3f6bffc55d266b874c59c27a0d3ed1936eed4
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-08-11 17:24:54 +0200

    added installer scripts

--- update.sh
+++ update.sh
@@ -9,3 +9,6 @@ wget $ARGONDOWNLOADSERVER/scripts/argonregister.py -O $SOURCE_DIR/argonregister.
 wget $ARGONDOWNLOADSERVER/scripts/argonsysinfo.py -O $SOURCE_DIR/argonsysinfo.py
 wget "$ARGONDOWNLOADSERVER/scripts/argonpowerbutton-${CHECKGPIOMODE}.py" -O $SOURCE_DIR/argonpowerbutton.py
 
+wget $ARGONDOWNLOADSERVER/argon1.sh -O $SOURCE_DIR/installers/argon1.sh
+wget $ARGONDOWNLOADSERVER/argoneon.sh -O $SOURCE_DIR/installers/argoneon.sh
+
