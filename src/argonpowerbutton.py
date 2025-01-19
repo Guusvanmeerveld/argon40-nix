e9b5e3ba05461cdf2f2762876fe386c513828125
Guusvanmeerveld <57101832+Guusvanmeerveld@users.noreply.github.com>
2025-01-19 01:49:09 +0000

    Auto-update: files have changed

--- src/argonpowerbutton.py
+++ src/argonpowerbutton.py
@@ -52,3 +52,38 @@ def argonpowerbutton_monitor(writeq):
 		chip.close()
 	except Exception:
 		writeq.put("ERROR")
+
+
+def argonpowerbutton_monitorswitch(writeq):
+
+	try:
+		# Reference https://github.com/brgl/libgpiod/blob/master/bindings/python/examples/gpiomon.py
+
+		# Pin Assignments
+		LINE_SHUTDOWN=4
+		try:
+			# Pi5 mapping
+			chip = gpiod.Chip('4')
+		except Exception as gpioerr:
+			# Old mapping
+			chip = gpiod.Chip('0')
+
+		lineobj = chip.get_line(LINE_SHUTDOWN)
+		lineobj.request(consumer="argon", type=gpiod.LINE_REQ_EV_BOTH_EDGES)
+		while True:
+			hasevent = lineobj.event_wait(10)
+			if hasevent:
+				pulsetime = 1
+				eventdata = lineobj.event_read()
+				if eventdata.type == gpiod.LineEvent.RISING_EDGE:
+					# Time pulse data
+					while lineobj.get_value() == 1:
+						time.sleep(0.01)
+						pulsetime += 1
+
+					if pulsetime >= 10:
+						writeq.put("OLEDSWITCH")
+		lineobj.release()
+		chip.close()
+	except Exception:
+		writeq.put("ERROR")
